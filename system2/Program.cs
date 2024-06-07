e2e29810c2cf268ee85ff21c2604d39d9c669a78
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-07 19:02:18 +0300

    update

--- system2/Program.cs
+++ system2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace system2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введіть початок діапазону: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть кінець діапазону: ");
+            int end = int.Parse(Console.ReadLine());
+
+           
+            Thread numberThread = new Thread(() => PrintNumbers(start, end));
+
+        
+            numberThread.Start();
+
+            
+            numberThread.Join();
+
+            Console.WriteLine("Всі числа виведено.");
+        }
+
+        static void PrintNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100); 
+            }
+        }
+    }
+}
